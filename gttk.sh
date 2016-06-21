48c25e99d9f4dd48a6f086471daa6b2c1e190a2c
dmustieles <daniel.mustieles@gmail.com>
2016-06-21 09:35:14 +0200

    Added initial support to commit gimp-help files

--- gttk.sh
+++ gttk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (C) 2012 - 2014 Daniel Mustieles <daniel.mustieles@gmail.com>
+# Copyright (C) 2012 - 2016 Daniel Mustieles <daniel.mustieles@gmail.com>
 
 #
 # This program is free software; you can redistribute it and/or modify
@@ -10,17 +10,17 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, see <http://www.gnu.org/licenses/>
 #
 
 
 # Carpeta donde estan los archivos .po listos para subir a Git
 GTTK_UPLOAD="/home/leo/po-files/subir"
+GTTK_GIMP_UPLOAD="/home/leo/po-files/subir/gimp"
 
 # Carpeta donde tenemos los clones de git
 GTTK_GIT_CLONES="/home/leo/gnome"
@@ -481,3 +481,4 @@ case $OPCION in
 		CommitPO
 	;;
 esac
+
