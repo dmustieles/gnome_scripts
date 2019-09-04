f1e902d784d13bfb410d60dd1b7b5b984def946e
dmustieles <daniel.mustieles@gmail.com>
2019-09-04 15:06:57 +0200

    New features in report.sh script
    Here they are:
        - Reports are separated in modulesets for every language
        - Separate folders for every language
        - Included gnome-extras stable moduleset
        - Code cleanup

--- report.sh
+++ report.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (C) 2015 Daniel Mustieles, <daniel.mustieles@gmail.com>
+# Copyright (C) 2019 Daniel Mustieles, <daniel.mustieles@gmail.com>
 
 #
 # This program is free software; you can redistribute it and/or modify
@@ -18,51 +18,68 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
-RELEASE="gnome-3-32"
+RELEASE="gnome-3-34"
 DL_URL="https://l10n.gnome.org/languages"
 WGET_OPTS="wget --no-check-certificate -q"
 
 if [ $# -eq 0 ]
 then
-	LINGUAS="ar bg bn_IN ca cs da de el en_GB es eu fa fi fr gl gu hi hu id it ja ko lv mk nds nl oc pa pl pt pt_BR ro ru sl sr sr@latin sv ta te th tr uk vi zh_CN zh_HK zh_TW"
+	LINGUAS="ar bg bn_IN ca cs da de el en_GB es eu fa fi fr gl gu hi hr hu id it ja ko lv mk nds nl oc pa pl pt pt_BR ro ru sl sr sr@latin sv ta te th tr uk vi zh_CN zh_HK zh_TW"
 else
 	LINGUAS=$@
 fi
 
-# Get all the documentation PO file from Damned-Lies, using languages with at least one string in DL
+# Get all the documentation PO files from Damned-Lies, using languages with at least one string in DL
 for i in `echo $LINGUAS`
 do
 	echo -e "Downloading PO files for:\e[1;32m $i \e[0m"
-	mkdir $i
+	mkdir -p $i/core $i/infrastructure $i/gimp $i/gnome-extras $i/gnome-extras-stable
 
-	$WGET_OPTS $DL_URL/$i/$RELEASE/doc.tar.gz -O $i/$i.tar.gz ; tar -zxf $i/$i.tar.gz -C $i ; rm $i/$i.tar.gz
-	$WGET_OPTS $DL_URL/$i/gnome-infrastructure/doc.tar.gz -O $i/infraestructure.tar.gz && tar -zxf $i/infraestructure.tar.gz -C $i && rm $i/infraestructure.tar.gz
-	$WGET_OPTS $DL_URL/$i/gnome-gimp/doc.tar.gz -O $i/gimp.tar.gz && tar -zxf $i/gimp.tar.gz -C $i && rm $i/gimp.tar.gz
-	$WGET_OPTS $DL_URL/$i/gnome-extras/doc.tar.gz -O $i/extras.tar.gz && tar -zxf $i/extras.tar.gz -C $i && rm $i/extras.tar.gz
+	$WGET_OPTS $DL_URL/$i/$RELEASE/doc.tar.gz -O $i/core/$i.tar.gz ; tar -zxf $i/core/$i.tar.gz -C $i/core ; rm $i/core/$i.tar.gz
+	$WGET_OPTS $DL_URL/$i/gnome-infrastructure/doc.tar.gz -O $i/infrastructure/infrastructure.tar.gz && tar -zxf $i/infrastructure/infrastructure.tar.gz -C $i/infrastructure && rm $i/infrastructure/infrastructure.tar.gz
+	$WGET_OPTS $DL_URL/$i/gnome-gimp/doc.tar.gz -O $i/gimp/gimp.tar.gz && tar -zxf $i/gimp/gimp.tar.gz -C $i/gimp && rm $i/gimp/gimp.tar.gz
+	$WGET_OPTS $DL_URL/$i/gnome-extras/doc.tar.gz -O $i/gnome-extras/extras.tar.gz && tar -zxf $i/gnome-extras/extras.tar.gz -C $i/gnome-extras && rm $i/gnome-extras/extras.tar.gz
+
+	$WGET_OPTS $DL_URL/$i/gnome-extras-stable/doc.tar.gz -O $i/gnome-extras-stable/gnome-extras-stable.tar.gz && tar -zxf $i/gnome-extras-stable/gnome-extras-stable.tar.gz -C $i/gnome-extras-stable && rm $i/gnome-extras-stable/gnome-extras-stable.tar.gz
 done
+
 
 # Check PO files with gtxml and create a report file for each language
 echo -e "\nGenerating report..."
 
-for j in `echo $LINGUAS`
+for lang in `echo $LINGUAS`
 do
-	gtxml $j/*.po >>$j-report.txt
-	rm -rf $j
+	# Remove POT files and folders with only POT files
+	find $lang -iname *.pot -exec rm {} \;
+	find $lang -type d -empty -exec rm -r {} \; >/dev/null 2>&1
 
-	# Remove files with zero size (language with no errors) and pack all reports in a .tar.gz file, ready to send to i18n mail list
-	find . -size 0 -exec rm {} \;
+	for moduleset in `ls $lang`
+	do
+		gtxml $lang/$moduleset/*.po >> $lang/$moduleset-$lang.txt
+	done
 
-	# If there is any .txt file it means we've found errors, so report must be generated
-	if [ -f $j-report.txt ]
+	rm -rf $lang/core $lang/infrastructure $lang/gimp $lang/gnome-extras $lang/gnome-extras-stable
+done
+
+# Remove empty files/folders (language with no errors)
+find . -size 0 -exec rm {} \;
+find . -type d -empty -exec rm -r {} \; >/dev/null 2>&1
+
+# Pack all reports in a .tar.gz file, ready to send to i18n mail list
+for i in `ls -d */`
+do
+	if [ -n `ls -A $i >/dev/null 2>&1` ]
 	then
-		tar -rf gtxml-doc-reports.tar *.txt
-		rm *.txt
+		tar -rf gtxml-doc-reports.tar $i
+		rm -rf $i
+	else
+		rm -rf $i
 	fi
 done
 
 gzip gtxml-doc-reports.tar
 
-REP_LIST=`tar -tf gtxml-doc-reports.tar.gz |awk -F '-' {'print $1'}`
+REP_LIST=`tar -tf gtxml-doc-reports.tar.gz |awk -F '/' {'print $1'} | uniq`
 
 echo -e "\nThis is the list of the affected languages:"
 echo -e "\e[1;31m$REP_LIST \e[0m\n"
